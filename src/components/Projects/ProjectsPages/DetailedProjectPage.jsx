c236cb73c5ab518794a72491ef2f49a379d37cd9
ThomasPiessat <thomasdidier30@gmail.com>
2023-12-26 19:03:26 +0100

    Add sidebar to detailedProject + Create style.css

--- src/components/Projects/ProjectsPages/DetailedProjectPage.jsx
+++ src/components/Projects/ProjectsPages/DetailedProjectPage.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams , Link } from 'react-router-dom';
 import { Slide } from 'react-slideshow-image';
 import './DetailedProjectPage.css'
 
 function DetailedProjectPage({ projects }) {
-  
-    // Find the project based on category and projectId
-    const project = projects.find((p) => p.projectType === "StudentProject" || p.projectType === "ProfessionalProject");
-  
-    // If project is not found
-    if (!project) {
-      return <div>Project not found</div>;
-    }
-  
-    return (
-      <div className="detailed-project">
+  const { title } = useParams();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  // Find the project based on title
+  const project = projects.find((p) => p.title === title);
+
+  // If project is not found
+  if (!project) {
+    return <div>Project not found</div>;
+  }
+
+  return (
+    <div className="detailed-project">
+      <div className="sidebar">
+        <h2>All Projects</h2>
+        <ul>
+          {projects.map((p) => (
+            <li key={p.title}>
+              <Link to={`/project/${p.title}`}>{p.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="content">
         <h1>{project.title}</h1>
         <div className="detailed-slide-container">
           <Slide indicators={true} images={project.images.map((image) => ({ source: image }))} autoplay duration={5000}>
@@ -24,31 +41,35 @@ function DetailedProjectPage({ projects }) {
             ))}
           </Slide>
         </div>
-        <h3>
-          <strong><u>Introduction:</u></strong>
-        </h3>
-        <p>
-          <strong>Project Type:</strong> {project.projectType}
-        </p>
-        <p>
-          <strong>Engine:</strong> {project.engine}
-        </p>
-        <p>
-          <strong>Language:</strong> {project.language}
-        </p>
-        <p>
-          <strong>Team:</strong> {project.team}
-        </p>
-        <p>
-          <strong>Time:</strong> {project.time}
-        </p>
-        <p>
-          <strong>Period:</strong> {project.period}
-        </p>
-        <h3>
-          <strong><u>About the Game:</u></strong> 
-          <p>{project.aboutTheGame}</p>
-        </h3>
+          <h3>
+            <strong><u>Introduction:</u></strong>
+          </h3>
+          <p>
+            <strong>Project Type:</strong> {project.projectType}
+          </p>
+          <p>
+            <strong>Engine:</strong> {project.engine}
+          </p>
+          <p>
+            <strong>Language:</strong> {project.language}
+          </p>
+          <p>
+            <strong>Team:</strong> {project.team}
+          </p>
+          <p>
+            <strong>Time:</strong> {project.time}
+          </p>
+          <p>
+            <strong>Period:</strong> {project.period}
+          </p>
+          <h3>
+            <strong><u>About the Game:</u></strong> 
+            <p>{project.aboutTheGame}</p>
+          </h3>
+        </div>
+        <div className="back-button">
+          <Link to="/project">Back to Projects View</Link>
+        </div>
       </div>
     );
   }
