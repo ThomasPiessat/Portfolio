ba6cc94ebe3abfd810d3cf09cdd799a7eba19ead
ThomasPiessat <thomasdidier30@gmail.com>
2025-10-28 16:32:24 +0100

    Update profileData.js

--- src/profileData.js
+++ src/profileData.js
@@ -2,5 +2,5 @@ export const profile = {
   name: "Thomas Piessat",
   role: "Software Engineer",
   company: { name: "LVCIM", url: "https://www.lvcim.com/" },
-  tagline: "Tools & plugins, UE5/Unity, C++/C#",
+  tagline: "Tools & plugins, UE5, C++",
 };
